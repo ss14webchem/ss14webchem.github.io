e050e34c5536b6e7b0680c7105e5a5bf0168b738
August Eymann <august.eymann@gmail.com>
2024-09-15 08:53:48 -0700

    did it all mostly..

---
+++ src/components/chem.svelte
@@ -0,0 +1,79 @@
+<script>
+    export let title;
+    export let reactants;
+    export let products;
+    export let minTemp;
+    let product;
+    let react = Object.entries(reactants);
+    if (products) {
+        product = Object.entries(products);
+    }
+
+
+    import { Card } from 'agnostic-svelte';
+</script>
+
+<div>
+    <Card isBorder="{true}" isStacked="{true}">
+        
+        <b>
+            <div class="p16 title">{ title }</div>
+        </b>
+
+        <div class="split">
+            <div class="p16 list">
+                <b>
+                    <div class="p16 tag">Reactants</div>
+                </b>
+                {#each react as reactant}
+                    <div class="p16 item">
+                        {reactant[0]} [{reactant[1].amount}]
+                    </div>
+                {/each}
+            </div> 
+            {#if products}
+                <div class="p16 list">
+                    <b>
+                        <div class="p16 item">Products</div>
+                    </b>
+                    {#each product as prod}
+                        <div class="p16 item">
+                            {prod[0]} [{prod[1]}]
+                        </div>
+                    {/each}
+                </div> 
+            {/if}
+
+            </div>
+        <div class="p16 hidden">🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀🧑‍🚀</div>
+    </Card>
+</div>
+
+<style>
+    .item {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .list {
+        display: flex;
+        flex-direction: column;
+    }
+        
+    .split {
+        display: inline-flex;
+    }
+
+
+    .title {
+        font-size: 20px;
+    }
+
+    .tag {
+        font-size: 18px;
+    }
+
+   .hidden {
+    visibility: hidden;
+   }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,104 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import { get } from 'svelte/store';
+
+    import 'agnostic-svelte/css/common.min.css';
+    import { Header, Input,HeaderNav,HeaderNavItem} from 'agnostic-svelte';
+    import Chem from '../components/chem.svelte';
+    let filterPromise;
+
+    let searchTerm = ''
+    $: {
+        console.log(searchTerm)
+        filterPromise = getFiltered()
+    }
+    // JSON
+    import chemicalsf from '../reactions/chemicals.json'
+    import biological from '../reactions/biological.json'
+    import botany from '../reactions/botany.json'
+    import cleaning from '../reactions/cleaning.json'
+    import fun from '../reactions/fun.json'
+    import medicine from '../reactions/medicine.json'
+    import pyrotechnic from '../reactions/pyrotechnic.json'
+    import gas from '../reactions/gas.json'
+    import food from '../reactions/food.json'
+    
+    let chemicals = chemicalsf.concat(fun).concat(medicine).concat(pyrotechnic).concat(gas).concat(biological).concat(cleaning).concat(botany)
+
+    async function getFiltered() {
+        if(searchTerm.length > 0) {
+            let filter = await chemicals.filter(data => {
+                return data.id.toLowerCase().includes(searchTerm.toLowerCase());
+            });
+            return filter
+        } else {
+            return chemicals
+        }
+    }
+
+    filterPromise = getFiltered();
+
+
+
+
+    //console.log(chemicals)
+
+
+
+
+	
+</script>
+
+<div class="root">
+    <div class="container">
+        <Header isHeaderContentEnd="{true}">
+            <a class="flex-fill" href="https://web.dev/">SS14WebChem</a>
+            <HeaderNav>
+                <HeaderNavItem>
+                    <!---<div><code>bind:value</code> = {searchTerm}</div> -->
+                    <Input bind:value={searchTerm}  uniqueId="2" isUnderlined isUnderlinedWithBackground isRounded placeholder="Search"/>
+                </HeaderNavItem>
+            </HeaderNav>
+        </Header>
+        <div class="container card-grid">
+
+                {#await filterPromise}
+                    Loading...
+                {:then chemicalz}
+                    {#each chemicalz as chemical}
+                        <Chem title={chemical.id} reactants={chemical.reactants} products={chemical.products} minTemp={chemical.minTemp}/>
+                    {/each}
+                {:catch someError}
+                    System error: {someError.message}
+                {/await}
+            </div>
+
+    </div>
+</div>
+
+<style>
+    .root {
+        font-family: var(--agnostic-font-family-body);
+        font-size: var(--agnostic-body);
+        color: var(--agnostic-font-color);
+        -webkit-font-smoothing: antialiased;
+        line-height: 1.5;
+    }
+
+
+    .container {
+        margin: 0 auto;
+    }   
+
+    .card-grid {
+        margin: auto;
+        gap: 1rem;
+        padding-top: 2rem;
+        padding-left: 5%;
+        display: flex;
+        flex-wrap: wrap;
+   }
+   
+
+   
+</style>
